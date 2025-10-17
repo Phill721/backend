12f1992449f0f431a0724879d237c90dcd705342
Phillipe <ge.bonhomme@duocuc.cl>
2025-10-17 12:51:07 -0300

    Agregados modelos del microservicio carrito

---
+++ Microservicio_carrito/src/main/java/com/huertohogarJV/backend/Models/Carrito.java
@@ -0,0 +1,40 @@
+package com.huertohogarJV.backend.Models;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Carrito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String usuario_id;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<ItemCarrito> items;
+
+    @Column
+    private Integer total;
+
+    public Integer calcularTotal(){
+        if (items == null) return 0;
+        return items.stream()
+                .mapToInt(ItemCarrito::getSubtotal)
+                .sum();
+    }
+}
